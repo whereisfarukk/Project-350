dd67b430374c8df5e0b481261e22dc4dfaa454d0
whereisfarukk <whereisfarukk@gmail.com>
2023-10-09 18:18:13 +0600

    restructure

--- public/script.js
+++ public/script.js
@@ -5,16 +5,17 @@ const alreadyMemberLink = document.querySelector(".signin-image-link");
 
 createAccountLink.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent the default link behavior
+
+  // Toggle the visibility of the sign-in and sign-up forms
   loginForm.classList.add("hidden");
   registrationForm.classList.remove("hidden");
+  // registrationForm.style.display =
+  //   registrationForm.style.display === "none" ? "block" : "none";
 });
 alreadyMemberLink.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent the default link behavior
+
+  // Toggle the visibility of the sign-in and sign-up forms
   loginForm.classList.remove("hidden");
   registrationForm.classList.add("hidden");
-});
-window.addEventListener("load", function () {
-  // Reset the form fields when the page loads
-  const loginForm = document.getElementById("login-form");
-  loginForm.reset();
 });
