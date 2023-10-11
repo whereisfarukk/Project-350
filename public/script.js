a6b90d8e2c5cea804aaee4057954ea9badcc91de
Asif032 <asif01981872223@gmail.com>
2023-10-11 19:20:27 +0600

    fixed form issue

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-const { application } = require("express");
-
 const loginForm = document.querySelector(".signin-content");
 const registrationForm = document.querySelector(".signup-content");
 const createAccountLink = document.querySelector(".signup-image-link");
